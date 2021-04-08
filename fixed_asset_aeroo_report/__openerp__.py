87442683219b56e5b9e4dc5746ab909d450c5331
Andhitia Rama <andhitia.r@gmail.com>
2021-04-08 11:30:46 +0700

    [fixed_asset_aeroo_report] 8.0.1.0.0

---
+++ fixed_asset_aeroo_report/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Fixed asset report",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "fixed_asset",
+        "report_aeroo",
+    ],
+    "data": [
+        "wizards/wizard_fixed_asset.xml",
+        "reports/report_fixed_asset_ods.xml",
+        "reports/report_fixed_asset_xls.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "license": "AGPL-3",
+}
