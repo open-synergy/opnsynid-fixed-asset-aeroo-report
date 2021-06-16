90981f9793a39457f61e6389abad497447bc7b3e
mikevhe18 <viriyananda.michael@gmail.com>
2021-06-16 13:23:23 +0700

    [fixed_asset_aeroo_report] 8.0.1.2.0
    Fix #8
    Fix #6

--- fixed_asset_aeroo_report/reports/report_fixed_asset.py
+++ fixed_asset_aeroo_report/reports/report_fixed_asset.py
@@ -10,11 +10,12 @@ from openerp.report import report_sxw
 class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
-        self.lines = []
+        self.no = 1
         self.localcontext.update(
             {
                 "time": time,
                 "get_date": self.get_date,
+                "get_category_ids": self.get_category_ids,
                 "line": self.get_line,
             }
         )
@@ -49,32 +50,56 @@ class Parser(report_sxw.rml_parse):
 
         return total_value
 
-    def get_line(self):
+    def get_category_asset_ids(self):
         obj_fixed_asset = self.pool.get("account.asset.asset")
 
         criteria = [("date_start", "<=", self.date), ("state", "in", ["open", "close"])]
 
+        asset_ids = obj_fixed_asset.search(
+            self.cr, self.uid, criteria, order="date_start"
+        )
+
+        asset = obj_fixed_asset.browse(self.cr, self.uid, asset_ids)
+
+        return asset.mapped("category_id").ids
+
+    def get_category_ids(self):
+        obj_asset_category = self.pool.get("account.asset.category")
+        category_ids = []
+
         if self.asset_category_ids:
-            criteria = [
-                ("category_id", "in", self.asset_category_ids),
-            ] + criteria
+            category_ids = self.asset_category_ids
+        else:
+            category_ids = self.get_category_asset_ids()
+
+        category_ids = obj_asset_category.browse(self.cr, self.uid, category_ids)
+
+        return category_ids
+
+    def get_line(self, category_id):
+        self.lines = []
+        obj_fixed_asset = self.pool.get("account.asset.asset")
+
+        criteria = [
+            ("date_start", "<=", self.date),
+            ("state", "in", ["open", "close"]),
+            ("category_id", "=", category_id),
+        ]
 
         asset_ids = obj_fixed_asset.search(
             self.cr, self.uid, criteria, order="date_start"
         )
 
         if asset_ids:
-            no = 1
             for asset in obj_fixed_asset.browse(self.cr, self.uid, asset_ids):
                 convert_dt = datetime.strptime(asset.date_start, "%Y-%m-%d")
                 salvage_value = self.get_salvage_value(asset)
                 total_value = self.get_total_value(asset)
                 asset_value = asset.purchase_value - salvage_value - total_value
                 res = {
-                    "no": no,
+                    "no": self.no,
                     "code": asset.code,
                     "name": asset.name,
-                    "category": asset.category_id.name,
                     "acquisition_value": asset.purchase_value,
                     "start_date": convert_dt.strftime("%d %B %Y"),
                     "age": asset.method_number,
@@ -84,6 +109,6 @@ class Parser(report_sxw.rml_parse):
                 }
 
                 self.lines.append(res)
-                no += 1
+                self.no += 1
 
         return self.lines
